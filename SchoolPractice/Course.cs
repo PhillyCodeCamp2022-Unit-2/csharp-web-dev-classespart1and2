f058eaa8cf0dd3c426eb0ea532534379cac135a0
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-04-22 14:39:42 -0500

    Created Course class, printed out info

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    internal class Course
+    {
+        public string Name { get; set; }
+        public string ProfessorName { get; set; }
+        public List<Student> StudentRoster { get; set; }
+
+        public Course(string name, string professor, List<Student> roster) 
+        {
+            Name = name;
+            ProfessorName = professor;
+            StudentRoster = roster;
+        }
+    }
+}
--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SchoolPractice
 {
@@ -8,10 +9,32 @@ namespace SchoolPractice
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
             Student student1 = new Student("Tom", 8, 1, 4.0);
-            Console.WriteLine($"Name: {student1.Name}");
-            Console.WriteLine($"ID: {student1.StudentId}");
-            Console.WriteLine($"Credits: {student1.NumberOfCredits}");
-            Console.WriteLine($"GPA: {student1.Gpa}");
+            //Console.WriteLine($"Name: {student1.Name}");
+            //Console.WriteLine($"ID: {student1.StudentId}");
+            //Console.WriteLine($"Credits: {student1.NumberOfCredits}");
+            //Console.WriteLine($"GPA: {student1.Gpa}");
+
+            Student student2 = new Student("Jenell", 4, 1, 4.0);
+            Student student3 = new Student("Francesca", 5, 1, 4.0);
+
+            List<Student> students = new List<Student>();
+            students.Add(student1);
+            students.Add(student2);
+            students.Add(student3);
+
+            Course latin = new Course("Latin 101", "Professor Bourdegard", students);
+            Console.WriteLine($"Name: {latin.Name}");
+            Console.WriteLine($"Professor: {latin.ProfessorName}");
+            Console.WriteLine($"Roster:");
+            foreach (Student student in latin.StudentRoster)
+            {
+                Console.WriteLine("---------");
+                Console.WriteLine($"Name: {student.Name}");
+                Console.WriteLine($"ID: {student.StudentId}");
+                Console.WriteLine($"Credits: {student.NumberOfCredits}");
+                Console.WriteLine($"GPA: {student.Gpa}");
+                Console.WriteLine("---------");
+            }
         }
     }
 }
