993285dca8768fcb2b7e66ea963a24c890e4a976
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-04-22 14:17:04 -0500

    Completed Student class

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,11 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student student1 = new Student("Tom", 8, 1, 4.0);
+            Console.WriteLine($"Name: {student1.Name}");
+            Console.WriteLine($"ID: {student1.StudentId}");
+            Console.WriteLine($"Credits: {student1.NumberOfCredits}");
+            Console.WriteLine($"GPA: {student1.Gpa}");
         }
     }
 }
